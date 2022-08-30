18865c84286bf6248584e9dbf74c64a225c6853d
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-08-29 21:18:59 -0500

    GetAlbumByID use case finished with some notes to check

---
+++ src/main/java/ec/com/reactive/music/album/usecases/GetAlbumByIdUseCase.java
@@ -0,0 +1,37 @@
+package ec.com.reactive.music.album.usecases;
+
+import ec.com.reactive.music.album.collection.Album;
+import ec.com.reactive.music.album.dto.AlbumDTO;
+import ec.com.reactive.music.album.mapper.AlbumMapper;
+import ec.com.reactive.music.album.repository.IAlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+/*Remember our Functional Interface Function? Well, Function always receive something and returns another,
+ * so this is the expected behavior that we need in this usecase. Also remember that to activate a Function
+ * you use the apply() method. So, when you do the implementation you will override that method and set up inside what
+ * do you want to truly implement.*/
+@Service
+public class GetAlbumByIdUseCase implements Function<String, Mono<AlbumDTO>> {
+
+    //Don't forget our default initializations: AlbumMapper and IAlbumRepository
+    @Autowired
+    private IAlbumRepository albumRepository;
+
+    @Autowired
+    private AlbumMapper albumMapper;
+
+    /*From now we will treat the Mono.error() on the respective route in AlbumRouter. So, let focus only in
+    * launch then alongside with the switchIfEmpty()*/
+    @Override
+    public Mono<AlbumDTO> apply(String s) {
+        return albumRepository
+                .findById(s)
+                .switchIfEmpty(Mono.error(new Throwable(HttpStatus.NOT_FOUND.toString())))
+                .map(album -> albumMapper.convertEntityToDTO().apply(album));
+    }
+}
