ba0e6e0cd9a3bf3cc15b239bee1368c7aa57591c
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-08-29 21:19:31 -0500

    Creation of a predicate to handle null attributes (there is extra notes explaining this of course)

--- src/main/java/ec/com/reactive/music/album/dto/AlbumDTO.java
+++ src/main/java/ec/com/reactive/music/album/dto/AlbumDTO.java
@@ -1,10 +1,14 @@
 package ec.com.reactive.music.album.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import ec.com.reactive.music.album.collection.Album;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 @Data
 @NoArgsConstructor
@@ -15,5 +19,13 @@ public class AlbumDTO {
     private String name;
     private String artist;
     private Integer yearRelease;
+
+    /*This predicate was created to verify if an attribute is not sent throught the postman's body, equally to said it is null
+    * I made use of final class Optional and its static method .ofNullable() and .isEmpty() to ensure all */
+    public static Predicate<AlbumDTO> thereIsNullAttributes(){
+        return albumDTO -> Optional.ofNullable(albumDTO.getName()).isEmpty()
+                || Optional.ofNullable(albumDTO.getArtist()).isEmpty()
+                || Optional.ofNullable(albumDTO.getYearRelease()).isEmpty();
+    }
 
 }
