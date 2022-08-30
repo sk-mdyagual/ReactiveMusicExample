e018dbddcbe0912a2e5339f193894d96f2beab82
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-08-29 21:20:06 -0500

    Refactoring of SaveAlbumUseCase. Check comments to understand it.

--- src/main/java/ec/com/reactive/music/album/usecases/SaveAlbumUseCase.java
+++ src/main/java/ec/com/reactive/music/album/usecases/SaveAlbumUseCase.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 @Service
 public class SaveAlbumUseCase implements SaveAlbum {
 
@@ -18,16 +20,20 @@ public class SaveAlbumUseCase implements SaveAlbum {
     @Autowired
     private AlbumMapper albumMapper;
 
+    /*I update it in order to manage scenarios when we sent an empty AlbumDTO object or some of the attributes is null
+    * Using the ternary operator I split both cases. On the router, I will manage the catch of the exception dropped by
+    * switchIfEmpty also the Mono.errors()*/
     @Override
     public Mono<AlbumDTO> applySaveAlbum(AlbumDTO albumDTO) {
-        return this.albumRepository
-                .save(albumMapper.convertDTOToEntity().apply(albumDTO))
-                .switchIfEmpty(Mono.error(new Throwable(HttpStatus.EXPECTATION_FAILED.toString())))
-                .map(album -> albumMapper.convertEntityToDTO().apply(album))
-                .onErrorResume(throwable -> {
-                    //albumDTO.setIdAlbum(throwable.getCause().toString());
-                    return Mono.just(albumDTO.toBuilder().idAlbum(null).build());
-                });
+        return !Objects.isNull(albumDTO)? //A static method in final class Objects that allows to check if albumDTO is null
+                !AlbumDTO.thereIsNullAttributes().test(albumDTO) ? //This is a predicate, I create it on AlbumDTO class
+                this.albumRepository
+                        .save(albumMapper.convertDTOToEntity().apply(albumDTO))
+                        .switchIfEmpty(Mono.error(new Throwable(HttpStatus.EXPECTATION_FAILED.toString())))
+                        .map(album -> albumMapper.convertEntityToDTO().apply(album))
+                : Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString()))
+        : Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString()));
+
     }
 
 
