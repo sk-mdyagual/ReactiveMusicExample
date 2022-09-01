e76c8bd564d41ab17f635aef5b61ee13445fd79d
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-08-31 19:20:35 -0500

    Refactoring to apply Constructor injection plus, the usage of other uses Cases instead of albumRepository

--- src/main/java/ec/com/reactive/music/album/usecases/UpdateAlbumUseCase.java
+++ src/main/java/ec/com/reactive/music/album/usecases/UpdateAlbumUseCase.java
@@ -4,6 +4,7 @@ import ec.com.reactive.music.album.dto.AlbumDTO;
 import ec.com.reactive.music.album.mapper.AlbumMapper;
 import ec.com.reactive.music.album.repository.IAlbumRepository;
 import ec.com.reactive.music.album.usecases.interfaces.UpdateAlbum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -15,30 +16,42 @@ import java.util.Objects;
 /*For update use case i create my own interface but it is possible to use a Function too as in GetAlbumByIdUseCase
 * because you will send something and return another something*/
 @Service
+@RequiredArgsConstructor
 public class UpdateAlbumUseCase implements UpdateAlbum {
 
-    @Autowired
-    private IAlbumRepository albumRepository;
+    //private final IAlbumRepository albumRepository;
 
-    @Autowired
-    private AlbumMapper albumMapper;
+    private final GetAlbumByIdUseCase getAlbumByIdUseCase;
+
+    private final SaveAlbumUseCase saveAlbumUseCase;
+
+    //private final AlbumMapper albumMapper;
 
     /*I'm considering again the scenarios when we sent an empty AlbumDTO object or some of the attributes is null
      * Using the ternary operator I split both cases. On the router, I will manage the catch of the exception dropped by
      * switchIfEmpty also the Mono.errors(). Remember: I will treat the Mono.error on the router. If I forgot it the usecase will fail*/
     @Override
     public Mono<AlbumDTO> applyUpdateAlbum(String id, AlbumDTO albumDTO) {
-        return !Objects.isNull(albumDTO)? //A static method in final class Objects that allows to check if albumDTO is null
+        //Refactor
+        /*return !Objects.isNull(albumDTO) ? //A static method in final class Objects that allows to check if albumDTO is null
                         !AlbumDTO.thereIsNullAttributes().test(albumDTO) ? //This is a predicate, I create it on AlbumDTO class
                         this.albumRepository
                                 .findById(id)
                                 .switchIfEmpty(Mono.error(new Throwable(HttpStatus.NOT_FOUND.toString())))
                                 .flatMap(album -> {
-                                    album.setIdAlbum(albumDTO.getIdAlbum());
-                                    return albumRepository.save(album).map(album1 -> albumMapper.convertEntityToDTO().apply(album1));
-                                }).switchIfEmpty(Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString())))
+                                    albumDTO.setIdAlbum(album.getIdAlbum());
+                                    return saveAlbumUseCase.applySaveAlbum(albumDTO);
+                                })//.map(album -> albumMapper.convertEntityToDTO().apply(album))
+                                .switchIfEmpty(Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString())))
                         : Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString()))
-                : Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString()));
+                : Mono.error(new Throwable(HttpStatus.NOT_ACCEPTABLE.toString()));*/
+
+        return getAlbumByIdUseCase
+                .apply(id)
+                .flatMap(albumDTOFound -> {
+                    Objects.requireNonNull(albumDTO).setIdAlbum(albumDTOFound.getIdAlbum());
+                    return saveAlbumUseCase.applySaveAlbum(albumDTO);
+                });
 
     }
 }
