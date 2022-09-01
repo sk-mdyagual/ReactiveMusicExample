97143b9290200909b0753ad8a6270670a714eac3
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-08-31 20:35:33 -0500

    DeleteSongByIdUseCase completed and functional

--- src/main/java/ec/com/reactive/music/song/usecases/DeleteSongByIdUseCase.java
+++ src/main/java/ec/com/reactive/music/song/usecases/DeleteSongByIdUseCase.java
@@ -3,6 +3,7 @@ package ec.com.reactive.music.song.usecases;
 import ec.com.reactive.music.album.mapper.AlbumMapper;
 import ec.com.reactive.music.song.repositories.ISongRepository;
 import ec.com.reactive.music.song.usecases.interfaces.DeleteSong;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -10,19 +11,17 @@ import reactor.core.publisher.Mono;
 /*In order to check a deleteUseCase I did it for Song entity
 * As a personal practice with delete operations i like to return something, and that will be the id*/
 @Service
+@RequiredArgsConstructor
 public class DeleteSongByIdUseCase implements DeleteSong {
-    @Autowired
-    private ISongRepository songRepository;
 
-    @Autowired
-    private AlbumMapper albumMapper;
+    private final ISongRepository songRepository;
 
     @Override
     public Mono<String> apply(String id) {
         return this.songRepository
                 .findById(id)
                 .switchIfEmpty(Mono.error(new Throwable(HttpStatus.NOT_FOUND.toString())))
-                .flatMap(album -> this.songRepository.delete(album))
+                .flatMap(this.songRepository::delete)
                 .thenReturn(id);
     }
 }
