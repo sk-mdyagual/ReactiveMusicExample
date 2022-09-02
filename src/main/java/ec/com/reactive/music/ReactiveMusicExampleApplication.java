3651ae2064556bd9e494cbb93436c1fd9a1738dd
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-09-02 18:37:44 -0500

    MainApp refactoring before adding swagger-ui

--- src/main/java/ec/com/reactive/music/ReactiveMusicExampleApplication.java
+++ src/main/java/ec/com/reactive/music/ReactiveMusicExampleApplication.java
@@ -1,12 +1,9 @@
 package ec.com.reactive.music;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Swagger Reactive music API", version = "1.0", description = "Documentation APIs v1.0"))
 public class ReactiveMusicExampleApplication {
 
 	public static void main(String[] args) {
